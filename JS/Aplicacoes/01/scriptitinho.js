fffff56d07cc4e325264c0672554acdfa779c25c
João Vi-Vi Neto <133411181+JoaoVitorinoVN@users.noreply.github.com>
2023-08-21 22:11:23 -0300

    Update scriptitinho.js
    Otimizando o script JS da AP-01 - JavaScript Básico

--- JS/Aplicacoes/01/scriptitinho.js
+++ JS/Aplicacoes/01/scriptitinho.js
@@ -1,14 +1,8 @@
 var nome = prompt(`Insira o seu nome`);
-var palavras = 1;
-for (var i = 0; i < nome.length; i++) {
-    if (nome[i] == " ") {
-        palavras++;
-    }
-}
 document.write(`<p>Seu nome possui ${nome.length} caracteres</p>\n
                 <p>A primeira letra do seu nome é ${nome.charAt(0)}</p>\n
                 <p>Seu nome convertido para letras maiúsculas: ${nome.toUpperCase()}</p>\n
-                <p>Seu nome possui ${palavras} palavras</p>\n
+                <p>Seu nome possui ${nome.split(" ").length} palavras</p>\n
                 <p>Seu nome <span style="color: red">${nome}</span> escrito na cor vermelho</p>\n
                 <p>Seu nome escrito 5 vezes na cor azul:</p>\n
                 <ul>\n`);
