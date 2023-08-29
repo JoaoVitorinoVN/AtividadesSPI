3de0822c4dd7236ef20de4570d7dff179291f33a
João Vi-Vi Neto <133411181+JoaoVitorinoVN@users.noreply.github.com>
2023-08-28 21:03:49 -0300

    Update script.js
    pequena fix para o ex11 lista 02

--- JS/Lista02/ex11/script.js
+++ JS/Lista02/ex11/script.js
@@ -1,5 +1,7 @@
 texto = prompt(`Insira um texto ;)`)
 pal1 = prompt(`Insira uma palavra legal que tava nesse texto aí`)
 pal2 = prompt(`Informe uma segunda palavra só para ser ainda mais legal que não tava lá, kkkk`)
-
-alert(`Texto alterado:\n${texto.replace(pal1, pal2)}`)
+for (i = 0; i < texto.length; i++) {
+    texto = texto.replace(pal1, pal2)
+}
+alert(`Texto alterado:\n${texto}`)
