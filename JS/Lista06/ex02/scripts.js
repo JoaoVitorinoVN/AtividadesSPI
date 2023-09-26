79f9ebb722f7a1c5fcf8a34a46e309312ac63c0e
João Vi-Vi Neto <133411181+JoaoVitorinoVN@users.noreply.github.com>
2023-09-25 21:27:06 -0300

    Okay, fix final

--- JS/Lista06/ex02/scripts.js
+++ JS/Lista06/ex02/scripts.js
@@ -1,5 +1,9 @@
-votos = [0,0,0,0]
-
+votos = [0,0,0,0];  
+function firstLoad () {
+    for (var i = 0; i < 4; i++) {
+        document.getElementById(`votos${i}`).innerHTML = 0;
+    }
+}
 function toVote(id) {
     votos[id]++
     document.getElementById(`votos${id}`).innerHTML = votos[id];
