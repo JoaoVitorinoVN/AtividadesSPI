8f3253d18f7d4804f19d9f56451eac4dc5502948
João Vi-Vi Neto <133411181+JoaoVitorinoVN@users.noreply.github.com>
2023-10-16 14:14:06 -0300

    Lista 07 - tentativa de arrumar os erros de URL

--- JS/Lista07/JS/script.js
+++ JS/Lista07/JS/script.js
@@ -4,7 +4,7 @@ function acolherNome () {
     var nome = document.getElementById("nome").value;
     if (nome != null && nome != "" && nome != " " && nome.includes(" ")) {
         localStorage.setItem("nome", nome);
-        window.location.href = "/menu.html";
+        window.location.href = "JVVN-AtividadesSPI/JS/Lista07/menu.html";
     } else {
         alert("Nome inválido! Insira no mínimo duas palavras!")
     }
