04e5540b5d2e5bd0fb26ca72b7fbfd89739501b8
João Vitorino V. Neto <133411181+JoaoVitorinoVN@users.noreply.github.com>
2024-10-28 14:20:46 -0300

    Add files via upload

---
+++ Projeto_Microsserviços_NoSQL/user-api/src/main/java/iftm/joaovitorinovn/user_api/dto/UserDTO.java
@@ -0,0 +1,24 @@
+package iftm.joaovitorinovn.user_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+    @NotBlank(message = "CPF é obrigatório")
+    private String cpf;
+    private String endereco;
+    @NotBlank(message = "E-mail é obrigatório")
+    private String email;
+    private String telefone;
+    private LocalDateTime dataCadastro;
+}
