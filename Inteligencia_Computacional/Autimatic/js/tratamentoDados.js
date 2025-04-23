15b32e41c5aeef13da7a71555cb7f205794ec521
JoaoVitorinoVN <azzydreemurr19@gmail.com>
2025-04-23 20:53:47 -0300

    fix/Autimatic: Ajustando a lógica
    A lógica feita no programa do autimatic para a geração de tabelas mostrou-se completamente errada com os requerimentos da atividade

--- Inteligencia_Computacional/Autimatic/js/tratamentoDados.js
+++ Inteligencia_Computacional/Autimatic/js/tratamentoDados.js
@@ -2,36 +2,40 @@ var teachers = [];
 var subjects = [];
 
 fetch('../data/professores.json')
-    .then(response => response.json())
-    .then(data => {
-        teachers = Object.values(data);
-    })
-    .catch(erro => console.error('Erro ao carregar o JSON:', erro));
+  .then(response => response.json())
+  .then(data => {
+    teachers = Object.values(data);
+  })
+  .catch(erro => console.error('Erro ao carregar o JSON:', erro));
 
-function assignSubjects() {
-    for (let i = 0; i < 25; i++) {
-        subjects[i] = i
-    }
-}
+fetch('../data/materias.json')
+  .then(response => response.json())
+  .then(data => {
+    subjects = Object.values(data);
+  })
+  .catch(erro => console.error('Erro ao carregar o JSON:', erro));
 
-export function generateClass() {
-    assignSubjects();
-    if (teachers.length === 0) {
-        console.error('Professores ainda não carregados!');
-        return [];
-    }
-
-    const result = [];
-
-    for (let i = 0; i < 25; i++) {
-        const subject = subjects[i % subjects.length];
-        const teacher = teachers[generateRandomNumber(0, teachers.length - 1)];
-        result.push(`${teacher}: ${subject}`);
-    }
-
-    return result;
-}
 
 function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export function createMatrix() {
+  var pop = [];
+  for (let i = 0; i < 10; i++) {
+    pop[i] = [];
+    for (let j = 0; j < 100; j++) {
+      pop[i][j] = "Vago";
+    }
+  }
+
+  for (let i = 0; i < 100; i++) {
+    const t = generateRandomNumber(0,9);
+      pop[t][i] = `${teachers[t]}: ${subjects[generateRandomNumber(0,24)]}`
+  }
+  return console.log(pop);
+}
+
+document.querySelector(".generator button").addEventListener("click", () => {
+  createMatrix()
+});
